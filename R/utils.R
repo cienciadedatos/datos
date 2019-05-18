eea563c3e757d25e63661bb2192e7a3dd81a8f8d
edgararuiz <edgararuiz@gmail.com>
2019-05-18 18:59:48 -0500

    Switches to scripts for delayed eval

--- R/utils.R
+++ R/utils.R
@@ -9,17 +9,34 @@ fix_reference <- function(ref_path = "docs/reference") {
   writeLines(index, file.path(ref_path, "index.html"))
 }
 
-data_script <- function(script_path) {
+data_script <- function() {
+  script_path <- "data/data.R"
   pkg_path <- system.file("specs", package = "datos")
   specs <- list.files(pkg_path)
   asp <- lapply(file.path(pkg_path, specs), yaml::read_yaml)
   anm <- as.character(lapply(asp, function(x) x$df$name))
+  scripts <- system.file("scripts", package = "datos")
   code <- lapply(
     seq_along(anm),
     function(x)
-      paste0("delayedAssign('", anm[x], "', datos:::translate('", specs[x], "'))")
+      paste0(
+        "delayedAssign('", anm[x], "',
+        eval(parse(file.path(system.file('scripts','",
+        anm[x], ".txt', package = 'datos')))))"
+        )
   )
   code <- as.character(code)
   if (file.exists(script_path)) unlink(script_path, force = TRUE)
   writeLines(code, script_path)
+  unlink("inst/scripts", recursive = TRUE)
+  dir.create("inst/scripts")
+  script <- readLines("R/translate.R")
+  lapply(
+    seq_along(anm),
+    function(x)
+      writeLines(
+        c(script, paste0("translate('", specs[x], "')")),
+        con = file.path("inst/scripts", paste0(anm[x], ".txt"))
+      )
+  )
 }
--- tests/testthat/test-utils.R
+++
@@ -1,7 +0,0 @@
-context("utils")
-
-test_that("Data script", {
-  temp <- tempfile()
-  expect_silent(data_script(temp))
-  expect_silent(data_script(temp))
-})
