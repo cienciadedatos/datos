24335c394fc8a3b660dfe880994e09d59cc93980
Edgar Ruiz <edgar@rstudio.com>
2019-05-19 14:45:39 -0500

    Fixes tests

--- tests/testthat/test-translations.R
+++ tests/testthat/test-translations.R
@@ -4,19 +4,27 @@ get_all_specs <- function(base_path = "specs", package = "datos") {
   pkg_path <- system.file(package = package)
   specs <- list.files(file.path(pkg_path, base_path))
   spec_paths <- file.path(file.path(pkg_path, base_path, specs))
-  lapply(spec_paths, yaml::read_yaml)
+  ry <- lapply(spec_paths, yaml::read_yaml)
+  lapply(
+    seq_along(ry),
+    function(x)
+      list(
+        file = specs[[x]],
+        original = ry[[x]]$df$source,
+        df = ry[[x]]$df$name,
+        variables = ry[[x]]$variables,
+        trans = ry[[x]]$trans
+      )
+  )
 }
 
-all_specs <- get_all_specs()
-
 lapply(
-  all_specs,
+  get_all_specs(),
   function(x) {
-    test_that(paste0("Load from spec works for:", x$df$name), {
-      df <- eval(rlang::parse_expr(x$df$name))
-      orig <- eval(rlang::parse_expr(x$df$source))
+    test_that(paste0("Load from spec works for:", x$df), {
+      df <- translate(x$file)
+      orig <- eval(parse(text = x$original))
       expect_is(df, "data.frame")
-      expect_is(eval(parse(text = paste0("datos::", x$df$name))), "data.frame")
       expect_equal(names(df), as.character(lapply(x$variables, function(x) x$trans)))
       expect_equal(nrow(orig), nrow(df))
       expect_equal(ncol(orig), ncol(df))
