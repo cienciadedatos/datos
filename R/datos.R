52cee15f4d84b5da59388b6b2b7388b63d34cd04
Mauro Lepore <maurolepore@gmail.com>
2020-04-11 20:45:25 -0500

    Avoid Namespace in Imports field not imported from
    Relates to #108
    
    Avoid note as in https://github.com/hadley/r-pkgs/issues/203
    While inelegant, this seems to be the recommended solution.
    It solves the note in the subject of this commit for most
    of the problematic platforms. There is still one platform
    I'm not sure about (details below). But in any case, this
    commit brings us closer to the goal.
    
    DETAILS
    
    CRAN's check results at
    https://cran-archive.r-project.org/web/checks/2020/2020-02-20_check_results_datos.html
    
    I no longer see the note on rhub with:
    Platform:	Fedora Linux, R-devel, clang, gfortran
    https://builder.r-hub.io/status/datos_0.2.0.tar.gz-d06dd13075724881be4ee9da953cd7ce
    
    But seems to persist on rhub with
    Platform:	Debian Linux, R-devel, GCC
    https://builder.r-hub.io/status/datos_0.2.0.tar.gz-109732efb4d34774b3d1124954b63ba4
    Maybe this result is from an older run. The results take a long
    time to arrive and older and later runs might get mixed.

--- R/datos.R
+++ R/datos.R
@@ -5,3 +5,16 @@
 #' @import rlang
 #' @keywords internal
 "_PACKAGE"
+
+# Avoid CMD check note Namespace in Imports field not imported from
+# https://github.com/hadley/r-pkgs/issues/203
+#' @import babynames
+#' @import fueleconomy
+#' @import nycflights13
+#' @import nasaweather
+#' @import Lahman
+#' @import forcats
+#' @import gapminder
+#' @import ggplot2
+#' @import tidyr
+NULL
