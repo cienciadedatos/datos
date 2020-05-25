bbb12d67fdeb26690f33bdedd642a633f18a09ee
Mauro Lepore <maurolepore@gmail.com>
2020-05-25 14:02:57 -0500

    Avoid R CMD check note

--- R/datos.R
+++ R/datos.R
@@ -1,7 +1,22 @@
-#' @importFrom yaml read_yaml
-#' @importFrom dplyr as_tibble
-#' @importFrom dplyr group_by
-#' @importFrom stats setNames
-#' @import rlang
 #' @keywords internal
 "_PACKAGE"
+
+#' @importFrom yaml read_yaml
+#' @importFrom dplyr as_tibble group_by
+#' @importFrom tibble as_tibble
+#' @importFrom stats setNames
+#' @import rlang
+NULL
+
+# Suppress R CMD check note
+# (e.g. https://github.com/tidyverse/tidyverse/blob/master/R/tidyverse.R#L4)
+#' @importFrom forcats fct_c
+#' @importFrom tidyr pivot_wider
+#' @importFrom ggplot2 ggplot
+#' @importFrom Lahman playerInfo
+#' @import babynames
+#' @import fueleconomy
+#' @import gapminder
+#' @import nasaweather
+#' @import nycflights13
+NULL
