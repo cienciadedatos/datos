f0b0790b998c6c730b30f099a6815d164588a38a
Mauro Lepore <maurolepore@gmail.com>
2020-06-28 13:31:33 -0500

    Remove dead code

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,15 +3,3 @@ context("utils")
 test_that("Site fix works", {
   expect_silent(fix_reference(tempdir(), TRUE))
 })
-
-test_that("Script generator works", {
-  expect_is(
-    data_script(
-      script_path = tempfile(),
-      script_target = tempdir(),
-      spec_path = system.file("specs", package = "datos"),
-      is_test = TRUE
-    ),
-    "list"
-  )
-})
